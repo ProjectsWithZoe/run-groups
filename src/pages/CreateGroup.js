751fec379bec6b642d0f259c67c36a4449bffb4c
Zoe Mwangi <your-email@example.com>
2025-04-30 16:18:05 +0100

    overflow fixed for main page

--- src/pages/CreateGroup.js
+++ src/pages/CreateGroup.js
@@ -30,7 +30,7 @@ function CreateGroup() {
   };
 
   return (
-    <div className="max-w-2xl mx-4">
+    <div className="max-w-2xl overflow-hidden mx-4">
       <h1 className="text-xl font-bold text-gray-900 my-4">Create your Running Group</h1>
       
       <form onSubmit={handleSubmit} className="space-y-2">
